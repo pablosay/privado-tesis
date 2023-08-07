6f2516f1370a2e16004aed2d989b240bf2a73626
pablosay <19001434@galileo.edu>
2023-08-07 14:16:19 -0600

    Prueba modelo yolov8s

--- facerecog1.py
+++ facerecog1.py
@@ -1,23 +1,13 @@
 import time 
 import cv2
 from picamera2 import Picamera2
-import tensorflow as tf
-from tensorflow.keras.models import load_model
 import numpy as np
+from ultralytics import YOLO 
 
-def draw_rectange(image, x, y, w, h):
-	
-	cv2.rectangle(image, (x,y), (x + w, y + h), (0, 255, 0))
-	
-gpus = tf.config.experimental.list_physical_devices('GPU')
-for gpu in gpus:
-	tf.config.experimental.set_memory_growth(gpu, True)	
-	
-facetracker = load_model('facetracker3.h5')
+facerecognitionmodel = YOLO('frv8s.pt')
 
-cv2.startWindowThread()
 picam2 = Picamera2()
-confg = picam2.create_preview_configuration(main={"size": (2000, 2000), "format": "XRGB8888"})
+confg = picam2.create_preview_configuration(main={"size": (640, 640)})
 picam2.configure(confg)
 picam2.start()
 
@@ -25,26 +15,26 @@ while True:
 	
 	im = picam2.capture_array();
 	
-	im = cv2.resize(im, (600,600))
-	
 	rgb = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
 	
-	resized = tf.image.resize(rgb, (120,120))
+	rgb = cv2.resize(rgb, (640,640))
 	
-	yhat = facetracker.predict(np.expand_dims(resized/255, 0))
+	facepredictions = facerecognitionmodel(rgb)
 	
-	sample_coords = yhat[1][0]
-	
-	
-	if yhat[0] > 0.9:
+	for result in facepredictions[0].boxes.data.numpy():
 		
-		print("PAblo")
+		if(result[4] > 0.6): 
 		
-		cv2.rectangle(im, tuple(np.multiply(sample_coords[:2], [600, 600]).astype(int)), 
-						  tuple(np.multiply(sample_coords[2:], [600, 600]).astype(int)),
-						  (255,0,0), 2)
+			bbox = result[:4]
+			
+			x_min, y_min, x_max, y_max = map(int, bbox)
+			
+			cv2.rectangle(rgb, (x_min, y_min), (x_max, y_max), (0,255,0), 2)
+	
+	shape = str(rgb.shape)
 		
-	cv2.imshow('Prueba', im)
+	cv2.imshow('Prueba ' + shape, rgb)
+	
 	key = cv2.waitKey(1) & 0xFF
 		
 	if key == ord('q'):
